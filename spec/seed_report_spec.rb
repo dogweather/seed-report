401eaea87d59547d16e60bf7982e422eeb5442b8
Robb Shecter <robb@weblaws.org>
2015-10-08 03:07:51 -0700

    Passing spec

--- spec/seed_report_spec.rb
+++ spec/seed_report_spec.rb
@@ -2,10 +2,10 @@ require 'spec_helper'
 
 describe SeedReport do
   it 'has a version number' do
-    expect(SeedReport::VERSION).not_to be nil
+    expect( SeedReport::VERSION ).not_to be nil
   end
 
-  it 'does something useful' do
-    expect(false).to eq(true)
+  it 'has a #report_growth method' do
+    expect( SeedReport ).to respond_to(:report_growth)
   end
 end
