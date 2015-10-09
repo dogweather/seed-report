c064f40576da633faf4f5e94e09a9ebcfb5ed134
Robb Shecter <robb@weblaws.org>
2015-10-08 21:25:34 -0700

    Formatting and colorized text, refactoring

--- lib/seed_report.rb
+++ lib/seed_report.rb
@@ -4,24 +4,40 @@ module SeedReport
   class << self
     def for_model(model, &block)
       initial_count = model.count
-      print "#{heading(model)} #{initial_count_display(initial_count)}"
-
+      output_heading(model)
+      output_initial_count(initial_count)
       block.call
+      output_increased_count(model.count - initial_count)
+    end
 
-      increase = model.count - initial_count
-      puts final_count_display(increase)
+    def output_heading(model)
+      print heading(model)
+    end
+
+    def output_initial_count(count)
+      print initial_count_display(count)
+    end
+
+    def output_increased_count(count)
+      puts increased_count_display(count)
     end
 
     def heading(model)
-      sprintf("  %-19s", model.name)
+      sprintf("%-16s", model.name) + " "
     end
 
     def initial_count_display(count)
       "initial count: #{sprintf("%3d", count)}"
     end
 
-    def final_count_display(amount)
-      ", #{sprintf("%3s", amount)} created"
+    def increased_count_display(amount)
+      amount_str = sprintf("%3d", amount)
+      amount_colored = amount > 0 ? cyan(amount_str) : amount_str
+      ", #{amount_colored} created"
+    end
+
+    def cyan(a_string)
+      "\e[36m#{a_string}\e[0m"
     end
   end
 end
