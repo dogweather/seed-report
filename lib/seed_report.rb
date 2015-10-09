5dcd7a5865c7940bd893b1d7558510c73caf41dd
Robb Shecter <robb@weblaws.org>
2015-10-08 20:50:37 -0700

    specs for output, removed elipsis

--- lib/seed_report.rb
+++ lib/seed_report.rb
@@ -3,20 +3,21 @@ require "seed_report/version"
 module SeedReport
   class << self
     def for_model(model, &block)
-      print "#{heading(model)} #{initial_count_display(model)}"
       initial_count = model.count
+      print "#{heading(model)} #{initial_count_display(initial_count)}"
+
       block.call
+
       increase = model.count - initial_count
       puts final_count_display(increase)
     end
 
     def heading(model)
-      class_name = "#{model.table_name.gsub('_',' ').titleize}..."
-      sprintf("  %-19s", class_name)
+      sprintf("  %-19s", model.name)
     end
 
-    def initial_count_display(model)
-      "initial count: #{sprintf("%3d", model.count)}"
+    def initial_count_display(count)
+      "initial count: #{sprintf("%3d", count)}"
     end
 
     def final_count_display(amount)
--- spec/seed_report_spec.rb
+++ spec/seed_report_spec.rb
@@ -1,11 +1,47 @@
 require 'spec_helper'
 
+class MockModel
+  attr_accessor :count, :name
+end
+
+
 describe SeedReport do
+  before(:each) do
+    @apple_model = MockModel.new
+    @apple_model.count = 5
+    @apple_model.name = 'Apple'
+  end
+
+
   it 'has a version number' do
     expect( SeedReport::VERSION ).not_to be nil
   end
 
   it 'has a #for_model method' do
     expect( SeedReport ).to respond_to(:for_model)
+  end
+
+  it 'outputs the original count' do
+    expect {
+      SeedReport.for_model @apple_model do
+        @apple_model.count = 11  # Simulate creating instances
+      end
+    }.to output(/5/).to_stdout
+  end
+
+  it 'outputs the increase' do
+    expect {
+      SeedReport.for_model @apple_model do
+        @apple_model.count = 11  # Simulate creating instances
+      end
+    }.to output(/6/).to_stdout
+  end
+
+  it 'outputs the model name' do
+    expect {
+      SeedReport.for_model @apple_model do
+        @apple_model.count = 11  # Simulate creating instances
+      end
+    }.to output(/Apple/).to_stdout
   end
 end
